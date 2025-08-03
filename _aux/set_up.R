bbd9246f710fc37dadbf8db3448d6bc2d81a5e18
Javier <saugarlopezj@gmail.com>
2025-08-03 18:25:26 +0200

    nta-from-api-and webscrapping-tecniques: installing package control

--- _aux/set_up.R
+++ _aux/set_up.R
@@ -3,13 +3,21 @@
 
 ## Package load function ----
 load_package <- function(pkg) {
+  
+  # Assure to hace installed packages
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    message(sprintf("Installing package: %s", pkg))
+    install.packages(pkg)
+  }
+  
+  # Load package if is not in the global environment
   if (!pkg %in% loadedNamespaces()) {
     suppressPackageStartupMessages(library(pkg, character.only = TRUE))
   }
 }
 
 ## Set packages to load ----
-packages <- c("dplyr", "ggplot2", "readr")
+packages <- c("rvest", "tidyverse")
 
 # Load packages
 invisible(lapply(packages, load_package))
